42b4847f7315507173b8ab6064a12560bc35b3a0
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2012-03-16 00:19:15 +0400

    added IronPython wrapper

---
+++ ListCalculator/CalculatorKernel/Kernel/IronPythonIntegration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Scripting.Hosting;
+using IronPython.Hosting;
+
+namespace CalculatorKernel.Kernel {
+    public class IronPythonWrapper {
+        ScriptEngine engine;
+        ScriptScope scope;
+
+        public IronPythonWrapper() {
+            this.engine = Python.CreateEngine();
+            this.scope = Engine.CreateScope();
+        }
+        protected ScriptEngine Engine { get { return engine; } }
+        protected ScriptScope Scope { get { return scope; } }
+        public dynamic Execute(string expression) {
+            return Engine.Execute(expression, scope);
+        }
+        public T Execute<T>(string expression) {
+            return Engine.Execute<T>(expression, scope);
+        }
+        public void Reset() {
+            this.scope = Engine.CreateScope();
+        }
+    }
+}
---
+++ ListCalculator/CalculatorKernel/Tests/IronPythonIntegrationTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using CalculatorKernel.Kernel;
+using IronPython.Runtime;
+
+namespace CalculatorKernel.Tests {
+
+    [TestFixture]
+    public class IronPythonIntegrationTests {
+        IronPythonWrapper wrapper;
+
+        protected IronPythonWrapper Wrapper { get { return wrapper; } }
+        [TestFixtureSetUp]
+        public void FixtureSetUp() {
+            wrapper = new IronPythonWrapper();
+        }
+        [SetUp]
+        public void SetUp() {
+            wrapper.Reset();
+        }
+        [Test]
+        public void SimpleTest() {
+            dynamic x = Wrapper.Execute("1");
+            Assert.That(x, Is.EqualTo(1));
+        }
+        [Test]
+        public void FunctionTest() {
+            Wrapper.Execute(@"
+def foo():
+    return 2
+");
+            Assert.That(Wrapper.Execute("foo()"), Is.EqualTo(2));
+        }
+        [Test]
+        public void PreserveScopeTest() {
+            Wrapper.Execute("x = 2");
+            Assert.That(Wrapper.Execute("x"), Is.EqualTo(2));
+            Wrapper.Execute("x = 3");
+            Assert.That(Wrapper.Execute("x"), Is.EqualTo(3));
+        }
+        [Test]
+        public void ResetScopeTest() {
+            Wrapper.Execute("x = 2");
+            Assert.That(Wrapper.Execute("x"), Is.EqualTo(2));
+            Wrapper.Reset();
+            Assert.That(() => Wrapper.Execute("x"), Throws.TypeOf<UnboundNameException>());
+        }
+    }
+}
