d604d3178f8f3ab23c83863eced572558397495d
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2012-03-16 22:48:48 +0400

    temporary nunit downgrade to 2.5 added kernel

---
+++ ListCalculator/CalculatorKernel/Kernel/Kernel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics.Contracts;
+using System.Threading.Tasks;
+
+namespace CalculatorKernel.Kernel {
+
+    public interface IKernel {
+        void StartCalculating(string expression);
+        event EventHandler<CalculationCompletedEventArgs> CalculationCompleted;
+    }
+
+    public interface ICalculationResult {
+        string PlainText { get; }
+    }
+
+    public class Kernel : IKernel {
+        IronPythonWrapper pythonWrapper = new IronPythonWrapper();
+        public event EventHandler<CalculationCompletedEventArgs> CalculationCompleted = delegate { };
+
+        public void StartCalculating(string expression) {
+            new Task(() => {
+                var result = this.pythonWrapper.Execute(expression);
+                CalculationCompleted(this, new CalculationCompletedEventArgs(CalculationResult.Create(result)));
+            }).Start();
+        }
+    }
+
+    public static class CalculationResult {
+        public static CalculationResult<T> Create<T>(T value) {
+            return new CalculationResult<T>(value);
+        }
+    }
+    public class CalculationResult<T> : ICalculationResult {
+        readonly T value;
+        string plainText = null;
+
+        public CalculationResult(T value) {
+            this.value = value;
+        }
+        public T Value { get { return value; } }
+        public string PlainText { get { return plainText ?? (plainText = Value.ToString()); } }
+    }
+
+    public class CalculationCompletedEventArgs : System.EventArgs {
+        readonly ICalculationResult result;
+
+        public CalculationCompletedEventArgs(ICalculationResult result) {
+            this.result = result;
+        }
+        public ICalculationResult Result { get { return result; } }
+    }
+}
---
+++ ListCalculator/CalculatorKernel/Tests/KernelTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Threading;
+
+namespace CalculatorKernel.Tests {
+    [TestFixture]
+    public class KernelTests {
+        Kernel.Kernel kernel;
+
+        Kernel.Kernel Kernel { get { return kernel; } }
+        [TestFixtureSetUp]
+        public void FixtureSetUp() {
+            kernel = new Kernel.Kernel();
+        }
+        [Test]
+        public void SimpleTest() {
+            TestCalculationPlainText("2", "2");
+        }
+        [Test]
+        public void FunctionTest() {
+            TestCalculationPlainText(@"
+def foo():
+    return 2
+foo()
+            ", "2");
+        }
+        [Test]
+        public void FactorialTest() {
+            TestCalculationPlainText(@"
+def factorial(n):
+    if(n <= 0):
+        return 1
+    return n * factorial(n - 1)
+factorial(5)
+            ", "120");
+        }
+        void TestCalculationPlainText(string expression, string expectedResult) {
+            ManualResetEvent evt = new ManualResetEvent(false);
+            string actualResult = string.Empty;
+            EventHandler<Kernel.CalculationCompletedEventArgs> action = (s, e) => {
+                actualResult = e.Result.PlainText;
+                evt.Set();
+            };
+            kernel.CalculationCompleted += action;
+            kernel.StartCalculating(expression);
+            evt.WaitOne();
+            Assert.That(actualResult, Is.EqualTo(expectedResult));
+        }
+    }
+}
